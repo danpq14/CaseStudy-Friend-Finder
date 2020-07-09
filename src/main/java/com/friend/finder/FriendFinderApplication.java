9bf54b2c905cfefaf7e637b9acfa42b2810ac12a
danpq14 <61695301+danpq14@users.noreply.github.com>
2020-07-09 16:25:20 +0700

    Cấu hình cơ bản và thêm templates

---
+++ src/main/java/com/friend/finder/FriendFinderApplication.java
@@ -0,0 +1,14 @@
+package com.friend.finder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+public class FriendFinderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FriendFinderApplication.class, args);
+    }
+
+}
